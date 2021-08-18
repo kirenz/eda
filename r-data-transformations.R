2bad2b059116442182928bcb69fc9fc13d0483f3
kirenz <jan.kirenz@outlook.de>
2021-08-18 19:51:03 +0200

    added data loans

---
+++ r-data-transformations.R
@@ -0,0 +1,55 @@
+# Script to convert R data to Python
+
+setwd("~/Downloads")
+
+library(tidyverse)
+library(openintro)
+library(tidytuesdayR)
+
+
+# Stent data
+stent30 <- 
+  stent30 %>% 
+  mutate(time = "30 days")
+
+stent365 <- 
+  stent365 %>% 
+  mutate(time = "365 days")
+
+stent <- 
+  stent30 %>% 
+  bind_rows(stent365)
+
+write_csv(stent, "stent.csv")
+
+# Loan50 data
+loan50 <- loan50
+write_csv(loan50, "loan50.csv")
+
+# passwords data
+passwords <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')
+
+passwords <- 
+  passwords %>% 
+  slice_head(n=500)
+
+write_csv(passwords, "passwords.csv")
+
+
+
+
+loans_full_schema
+
+loans <- loans_full_schema %>%
+  mutate(application_type = as.character(application_type)) %>%
+  filter(application_type != "") %>%
+  mutate(
+    homeownership    = tolower(homeownership), 
+    homeownership    = fct_relevel(homeownership, "rent", "mortgage", "own"), 
+    application_type = fct_relevel(application_type, "joint", "individual")
+  ) 
+loans_individual_rent <- loans %>%
+  filter(
+    application_type == "individual",
+    homeownership    == "rent") %>%
+  nrow()
