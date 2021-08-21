a29349217d3c78a750cac02a4a06165274312c0f
kirenz <jan.kirenz@outlook.de>
2021-08-21 15:32:02 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -4,12 +4,12 @@ jupyter nbconvert 0*.ipynb --to slides --reveal-prefix reveal.js
 # Convert all HTML-files to PDF with headless Chrome
 # Issue: produces only horizontal pages
 
-alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+# alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
-for i in $(ls *.html); do 
-    chrome --headless --disable-gpu --print-to-pdf="$i".pdf "$i"
-done
+# for i in $(ls *.html); do 
+#    chrome --headless --disable-gpu --print-to-pdf="$i".pdf "$i"
+# done
 
 # List all HTML files
-ls *.html
+# ls *.html
 
